5bd0916b4fdad0d4f73de5e99f86ad0a74b5a271
Bravin Murega <tedmurega@gmail.com>
2024-09-23 13:18:01 +0300

    refractoring and file move

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, make_response
 from flask_migrate import Migrate
 from dotenv import load_dotenv
-from app.models import *
+from models import *
 import os
 from config import config
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
