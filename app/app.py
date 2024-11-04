739d5ab30596ca496527b720d3f1a5934c65752c
Bravin Murega <tedmurega@gmail.com>
2024-11-04 11:03:17 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ import os
 from app.config import config
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import timedelta
+from .template import *
 
 load_dotenv()
 
